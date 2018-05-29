157f6ba6682548b3acec69dc31ce495f86013149
Nizmauddin <nizamdevlabs@gmail.com>
2018-05-29 10:39:24 +0530

    Intial commit of this app

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title> DU | Data</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <style>
+
+.form-control{
+  
+    font-size: 12px;
+    height: 29px;
+}
+body{
+    font-size: 12px
+}
+        </style>
+        <script type="text/javascript">
+
+        </script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-stripped table-bordered table-hover">
+                        <thead class="thead-dark">
+                            <tr>
+<!--                                <td class="text-center">REF #</td>-->
+                                <td class="text-center">Date Of Implant</td>
+                                <td class="text-center">Physician</td>
+                                <td class="text-center">Country</td>
+                                <td class="text-center">Region</td>
+                                <td class="text-center">State</td>
+                                <td class="text-center">City</td>
+                                <td class="text-center">Hospital</td>
+                                <td class="text-center">Company</td>
+                                <td class="text-center">Service By</td>
+                                <td class="text-center">Model Name</td>
+                                <td class="text-center">Model No</td>
+                                <td class="text-center">Qty</td>
+                                <td class="text-center">ASP</td>
+                                <td class="text-center">Action</td>
+                                <td class="text-center"><input type="checkbox" name="check_all"/></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+<!--                                <td class="text-center">REF #</td>-->
+                                <td class="text-center"><input type="date" class="form-control" name="implantdate"></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control" ><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control" ><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                                <td class="text-center"><select class="form-control"><option>Choose</option></select></td>
+                               <td class="text-center"><input type="text" class="form-control" name="implantdate"></td>
+                               <td class="text-center"><input type="text" class="form-control" name="implantdate"></td>
+                                <td class="text-center">Action</td>
+                                <td class="text-center"><input type="checkbox" name="check_all"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
